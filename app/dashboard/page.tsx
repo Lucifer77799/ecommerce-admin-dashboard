edb6cd0eaa950f7c6d0b67d5de7d3fc165f51e49
Lucifer77799 <nikunjm2005@gmail.com>
2026-01-05 03:42:53 +0530

    Force dashboard to be fully dynamic on Vercel

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,7 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+export const fetchCache = "force-no-store";
+
 import { connectDB } from "@/lib/mongodb";
 import Product from "@/models/Product";
 import ProductActions from "./ProductActions";
